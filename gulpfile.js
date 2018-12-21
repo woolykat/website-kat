45273b5e59d6a6f68a134d5b02d2d69836ba297f
katrina doyle <katrinadoyle@katrinas-MBP.home>
2018-12-21 15:22:43 +0000

    final essay 17th

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
      gulp.task('sass', function(){
-       return gulp.src('app/scss/katrina.scss')
+       return gulp.src('app/scss/main.scss')
          .pipe(sass()) // Converts Sass to CSS with gulp-sass
          .pipe(gulp.dest('app/css'))
     });
